ce7809f925d27c9f0d1d333a302f7b831f6faa4a
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-17 17:03:09 +0200

    change header

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -61,34 +61,6 @@ export default {
 <template>
 
     <template>
-        <div class="container py-5 ">
-            <h1 class="pb-4 text-center fw-bold text-uppercase">Progetti</h1>
-
-            <div v-if="!isLoading" class="row gap-4 justify-content-center">
-                <ApartmentItem class="col-auto" v-for="currentApartment in apartments" :project="currentApartment">
-                </ApartmentItem>
-
-                <div id="pagination" class="d-flex justify-content-center p-0">
-
-                    <ul class="d-flex gap-2 p-0">
-
-                        <li v-for="link in apiLinks" v-html="link.label" @click="changeApiPage(link.label)"
-                            :class="link.label == apiPageNumber ? 'active' : ''">
-
-                        </li>
-
-                    </ul>
-
-                </div>
-
-            </div>
-            <div v-else>
-                <div class="spinner-border" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
-
-        </div>
 
     </template>
 
