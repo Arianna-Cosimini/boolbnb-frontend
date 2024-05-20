18d1d2acd0a5d979129d0c116bfc464c2e2029fb
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-20 17:02:20 +0200

    fix : messages

--- src/pages/MessageMe.vue
+++ src/pages/MessageMe.vue
@@ -1,12 +1,25 @@
 <script>
+
+import MessageForm from '../components/MessageForm.vue';
+
+
 export default {
-   
+    data() {
+        return {
+            
+        }
+    },
+   components: {
+    MessageForm
+   }
 }
 </script>
 
 <template>
     <div class="container">
         <h1>Contact Me</h1>
+
+        <MessageForm></MessageForm>
     </div>
     
 </template>
