a932a6e5f814ec10e9381139d48ece6809d114bb
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-20 22:47:08 +0200

    fix;homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,4 @@
+home page
 <script>
 
 import { store } from '../store.js';
@@ -5,6 +6,10 @@ import { store } from '../store.js';
 import axios from 'axios';
 import ApartmentItem from '../components/ApartmentItem.vue';
 import Searchbar from '../components/Searchbar.vue';
+import AppHeader from '../components/AppHeader.vue';
+
+// import AppApartments from '../pages/AppApartments.vue';
+
 
 
 export default {
@@ -14,9 +19,11 @@ export default {
   components: {
     ApartmentItem,
     Searchbar,
+    AppHeader
+    // AppApartments
   },
 
-  
+
   data() {
     return {
 
@@ -34,75 +41,101 @@ export default {
       activeAuto: false,
       lastPage: '',
       totalItems: 0,
+      addressFromSearch: '',
+      apartments: null,
+      userAddress: '',
 
 
     }
   },
 
   mounted() {
- 
+
     this.apiCall();
   },
 
 
+  created() {
+    this.getApiApartments();
+    // this.getApiHome()
+
+  },
+
+
   methods: {
-
-    getApiAddresses() {
-      axios
-        .get(`https://api.tomtom.com/search/2/search/${this.store.address}.json`, {
-          params: {
-            'key': 'RrNofIXHXhCLSto2sM1SEfvmA1AamCSs',
-            'countrySet': 'IT',
-            'lat': '44.4949',
-            'lon': '11.3426',
-            'radius': '20000',
-            'limit': '5',
-          }
-        })
-        .then(response => {
-          this.autocomplete = response.data.results;
-        });
-    },
-
     getApiApartments() {
-      if (this.autocomplete == '') {
-      }
-      else {
+      if (this.autocomplete.length > 0) {
         this.store.lat = this.autocomplete[0].position.lat;
         this.store.lon = this.autocomplete[0].position.lon;
       }
-      axios
-        .get(store.baseApiApartments, {
-          params: {
-            'range': this.store.range,
-            'lat': this.store.lat,
-            'lon': this.store.lon,
-          }
-        })
-        .then((response) => {
 
-          if (response.data.success == true) {
+      const address = this.store.address;
+      const lat = this.store.lat;
+      const lon = this.store.lon;
+      const url = `${this.store.baseApiApartments}?address=${address}&lat=${lat}&lon=${lon}`;
+
+      axios.get(url, {
+        params: {
+          'range': this.store.range,
+          'lat': this.store.lat,
+          'lon': this.store.lon,
+        }
+      })
+        .then((response) => {
+          if (response.data.success) {
             this.apartments = response.data.apartments;
             this.messageChecked = false;
-            // //  pagination
-            // this.numPages = response.data.apartments.last_page;
-            // this.store.filteredMap = false;
-            // this.cancelAddress();
-          }
-          else {
-            this.message;
+          } else {
             this.messageChecked = true;
           }
-
         });
     },
 
+    getUserLocation() {
+      // Codice per ottenere la posizione geografica dell'utente
+      // Ad esempio, puoi usare il navigatore web
+      try {
+        const position = this.getCurrentPosition();
+        this.store.lat = position.coords.latitude;
+        this.store.lon = position.coords.longitude;
+      } catch (error) {
+        console.error('Impossibile ottenere la posizione dell\'utente:', error);
+      }
+    },
+    getCurrentPosition() {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    },
 
+    filterApartmentsByDistance() {
+      // Filtra gli appartamenti in base alla distanza massima consentita (20 km)
+      const searchRadius = 20; // Raggio di ricerca di 20 km
 
-   
+      this.apartments = this.apartments.filter(apartment => {
+        const distance = this.calculateDistance(this.store.lat, this.store.lon, apartment.position.lat, apartment.position.lon);
+        return distance <= searchRadius;
+      });
+    },
+
+    calculateDistance(lat1, lon1, lat2, lon2) {
+      // Formula per calcolare la distanza tra due punti su una sfera (Haversine formula)
+      const R = 6371; // Raggio medio della Terra in km
+      const dLat = this.deg2rad(lat2 - lat1);
+      const dLon = this.deg2rad(lon2 - lon1);
+      const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distance = R * c;
+      return distance;
+    },
+
+    deg2rad(deg) {
+      return deg * (Math.PI / 180);
+    },
     apiCall() {
-
-      const url = this.store.baseApiHome + 'apartments';
+      const url = `${this.store.baseApiHome}apartments`;
       console.log('URL chiamata API:', url);
 
       axios.get(url, {
@@ -110,159 +143,145 @@ export default {
           page: this.apiPageNumber
         }
       }).then(res => {
-
         console.log(res)
-
-      
         this.apartments = res.data.results.data;
-
-        // salvo il totale dei progetti
         this.totalApartment = res.data.results.total;
         this.itemPage = res.data.results.per_page;
         this.lastPage = res.data.results.last_page;
-        
-
         this.apiLinks = res.data.results.links;
       })
     },
-       
-    },
-
-  
     changeApiPage(pageNumber) {
-
-
-      
       if (pageNumber == "&laquo; Previous" && this.apiPageNumber > 1) {
-
-
-        
         this.apiPageNumber--;
-
-     
-        //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
       } else if (pageNumber == "Next &raquo;" && this.apiPageNumber < (this.apiLinks.length - 2)) {
-        //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
-
-       
         this.apiPageNumber++;
-
       }
-      
-      if (!isNaN(pageNumber)) {
 
+      if (!isNaN(pageNumber)) {
         this.apiPageNumber = pageNumber;
       }
-       
 
-
-     
       this.apiCall();
-
     },
-
     takeAddress(address) {
       this.activeAuto = false;
-      return this.store.address = address;
+      this.store.address = address;
+      this.getApiApartments(); // Update apartments based on new address
     },
+    applyAddressFilter() {
+      this.getApiApartments();
+    }
+  },
 
+  computed: {
 
- 
-  created() {
-    this.getApiApartments();
+    filteredApartments() {
+      if (!this.apartments || this.apartments.length === 0) {
+        return [];
+      }
 
+      // Filtra gli appartamenti in base all'indirizzo fornito dall'utente
+      return this.apartments.filter(apartment => {
+        return apartment.address.includes(this.store.address);
+      });
+    }
   }
+
+
+
 
 }
 </script>
 
 
 <template>
-
-
-  <div class="container text-center">
-    <div class="row">
-      <div class="col-md-12 my_mini_jumbo d-flex flex-column align-content-center justify-content-center">
-        <h1 class="center-content fs-1">I Nostri Appartamenti</h1>
+  <div>
+    <AppHeader></AppHeader>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-12 my_mini_jumbo d-flex flex-column align-content-center justify-content-center">
+          <h1 class="center-content fs-1">I Nostri Appartamenti</h1>
+        </div>
       </div>
     </div>
+
+
+    <div class="container">
+      <div class="row">
+        <template v-if="filteredApartments.length > 0">
+          <ApartmentItem v-for="apartment in filteredApartments" :key="apartment.id" :apartment="apartment" />
+        </template>
+        <template v-else>
+          <p>{{ message }}</p>
+        </template>
+      </div>
+    </div>
+
+    <nav class="button-nav d-flex justify-content-center">
+      <vue-awesome-paginate :total-items="totalApartment" :items-per-page="itemPage" :max-pages-shown="lastPage"
+        v-model="apiPageNumber" :on-click="changeApiPage" active-page-class="active-page"
+        paginate-buttons-class="paginate-buttons" />
+    </nav>
   </div>
 
 
-
-  <div class="row">
-    <ApartmentItem v-for="currentApartment in apartments" :apartment="currentApartment" :key="currentApartment.id">
-    </ApartmentItem>
-  </div>
-
-
-
-
-
-  {/* <!-- {{ $apartment->cover_image ? asset('storage/' . $apartment->cover_image) : asset('placeholder/Placeholder.png') }} --> */}
-
-  <nav class="button-nav d-flex justify-content-center">
-
-    <vue-awesome-paginate :total-items="totalApartment" :items-per-page="itemPage" :max-pages-shown="lastPage"
-      v-model="apiPageNumber" :on-click="changeApiPage" active-page-class="active-page"
-      paginate-buttons-class="paginate-buttons" />
-
-  </nav>
-    
 
 </template>
+
 
 <style lang="scss">
 .my_mini_jumbo {
   height: 60vh;
 }
+
 .my_mini_jumbo {
   height: 60vh;
 }
 
 .button-nav {
-.button-nav {
+  .button-nav {
 
-  margin-top: 100px;
-  padding-top: 20px;
-  border-top: solid 1px gray;
+    margin-top: 100px;
+    padding-top: 20px;
+    border-top: solid 1px gray;
 
-  .pagination-container {
-    display: flex;
-    column-gap: 10px;
+    .pagination-container {
+      display: flex;
+      column-gap: 10px;
+    }
+
+    .paginate-buttons {
+      height: 40px;
+      width: 40px;
+      border-radius: 20px;
+      cursor: pointer;
+      background-color: rgba($color: #d6d6d6d6, $alpha: 0.3);
+      border: 1px solid rgb(255, 255, 255);
+      color: rgb(255, 255, 255);
+    }
+
+    .paginate-buttons:hover {
+      background-color: #d8d8d8;
+    }
+
+    .active-page {
+      background-color: #3498db;
+      border: 1px solid #3498db;
+      color: white;
+    }
+
+    .active-page:hover {
+      background-color: #2988c8;
+    }
+
   }
-
-  .paginate-buttons {
-    height: 40px;
-    width: 40px;
-    border-radius: 20px;
-    cursor: pointer;
-    background-color: rgba($color: #d6d6d6d6, $alpha: 0.3);
-    border: 1px solid rgb(255, 255, 255);
-    color: rgb(255, 255, 255);
-  }
-
-  .paginate-buttons:hover {
-    background-color: #d8d8d8;
-  }
-
-  .active-page {
-    background-color: #3498db;
-    border: 1px solid #3498db;
-    color: white;
-  }
-
-  .active-page:hover {
-    background-color: #2988c8;
-  }
-
-}
 }
 
 
 .card {
 
-  
+
   cursor: pointer;
 
   .my_img_size {
