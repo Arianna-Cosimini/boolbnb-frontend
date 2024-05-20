0d59860fb44c1dd2cb0ee3fe98e08c7c891c48be
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-20 22:47:51 +0200

    fix:appapartments

--- src/pages/AppApartments.vue
+++ src/pages/AppApartments.vue
@@ -1,23 +1,47 @@
+appapartments
+
 <script>
 import { store } from '../store.js';
 import ApartmentItem from '../components/ApartmentItem.vue';
 
 export default {
     name: 'AppApartments',
-    
+
+    props: {
+        apartments: Array,
+    },
+
     data() {
         return {
             store,
+
         }
     },
     components: {
         ApartmentItem,
+
     },
     methods: {
 
 
-    }
-}
+    },
+    computed: {
+        filteredApartments() {
+            if (!this.apartments || this.apartments.length === 0) {
+                return [];
+            }
+
+            const userAddress = this.store.address; // Ottieni l'indirizzo fornito dall'utente
+
+            return this.apartments.filter((apartment) => {
+                // Filtra gli appartamenti in base all'indirizzo fornito dall'utente
+                return apartment.address.toLowerCase().includes(userAddress.toLowerCase());
+            });
+        }
+    },
+
+    // ...
+};
 </script>
 
 <template>
@@ -29,13 +53,9 @@ export default {
         </div>
     </div>
 
-
     <div class="row">
-
         <template v-if="apartments && apartments.length > 0">
-            <ApartmentItem v-for="currentApartment in apartments" :apartment="currentApartment"
-                :key="currentApartment.id">
-            </ApartmentItem>
+            <ApartmentItem v-for="apartment in apartments" :key="apartment.id" :apartment="apartment" />
         </template>
         <template v-else>
             <p>Nessun risultato</p>
