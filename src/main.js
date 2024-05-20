c6b95899543dc5dd55be4112a78255b4187c1023
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-20 10:44:49 +0200

    font-awesome

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import VueAwesomePaginate from "vue-awesome-paginate";
 // import the necessary css file
 import "vue-awesome-paginate/dist/style.css";
 
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import {router} from "./router"
 
 createApp(App).use(router).use(VueAwesomePaginate).mount('#app')
