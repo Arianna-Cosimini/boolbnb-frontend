8492a17613827113ecfb1ad7f1307bcbee14dd3e
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-20 11:56:06 +0200

    created page for message

---
+++ src/pages/MessageMe.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+   
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Contact Me</h1>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from './pages/HomePage.vue';
 import SingleApartment from './pages/SingleApartment.vue';
-
+import MessageMe from './pages/MessageMe.vue';
+/* import MessageMe from './pages/MessageMe.Vue'; */
 const router = createRouter({
     history:createWebHistory(),
 
@@ -18,7 +19,12 @@ const router = createRouter({
             path: '/apartments/:slug',
             name:'single-apartment',
             component: SingleApartment
-        }
+        },
+        {
+            path:'/message-me',
+            name:'message-me',
+            component: MessageMe
+        },
     ]
 })
 
