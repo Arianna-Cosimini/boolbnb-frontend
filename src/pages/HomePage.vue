37d7a1c33d1d056c9ddf5e7ee2538dc5aee96830
vi_ser <vinceulisse@gmail.com>
2024-05-20 22:04:17 +0200

    fixed: home stylization

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -16,7 +16,7 @@ export default {
     Searchbar,
   },
 
-  
+
   data() {
     return {
 
@@ -40,7 +40,7 @@ export default {
   },
 
   mounted() {
- 
+
     this.apiCall();
   },
 
@@ -99,7 +99,7 @@ export default {
 
 
 
-   
+
     apiCall() {
 
       const url = this.store.baseApiHome + 'apartments';
@@ -113,61 +113,61 @@ export default {
 
         console.log(res)
 
-      
+
         this.apartments = res.data.results.data;
 
         // salvo il totale dei progetti
         this.totalApartment = res.data.results.total;
         this.itemPage = res.data.results.per_page;
         this.lastPage = res.data.results.last_page;
-        
+
 
         this.apiLinks = res.data.results.links;
       })
     },
-       
-    },
 
-  
-    changeApiPage(pageNumber) {
+  },
 
 
-      
-      if (pageNumber == "&laquo; Previous" && this.apiPageNumber > 1) {
+  changeApiPage(pageNumber) {
 
 
-        
-        this.apiPageNumber--;
 
-     
-        //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
-      } else if (pageNumber == "Next &raquo;" && this.apiPageNumber < (this.apiLinks.length - 2)) {
-        //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
-
-       
-        this.apiPageNumber++;
-
-      }
-      
-      if (!isNaN(pageNumber)) {
-
-        this.apiPageNumber = pageNumber;
-      }
-       
+    if (pageNumber == "&laquo; Previous" && this.apiPageNumber > 1) {
 
 
-     
-      this.apiCall();
 
-    },
-
-    takeAddress(address) {
-      this.activeAuto = false;
-      return this.store.address = address;
-    },
+      this.apiPageNumber--;
 
 
- 
+      //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
+    } else if (pageNumber == "Next &raquo;" && this.apiPageNumber < (this.apiLinks.length - 2)) {
+      //this.apiLinks.length - 2 corrisponde alla lunghezza dell'array a cui sottraggo il previous and next(da qui il -2)
+
+
+      this.apiPageNumber++;
+
+    }
+
+    if (!isNaN(pageNumber)) {
+
+      this.apiPageNumber = pageNumber;
+    }
+
+
+
+
+    this.apiCall();
+
+  },
+
+  takeAddress(address) {
+    this.activeAuto = false;
+    return this.store.address = address;
+  },
+
+
+
   created() {
     this.getApiApartments();
 
@@ -180,26 +180,35 @@ export default {
 <template>
 
 
-  <div class="container text-center">
-    <div class="row">
-      <div class="col-md-12 my_mini_jumbo d-flex flex-column align-content-center justify-content-center">
-        <h1 class="center-content fs-1">I Nostri Appartamenti</h1>
+  <div class="container-fluid text-center mt-5">
+
+    <div class="row px-5">
+      <div v-for="apartment in apartments" class="col-2 mt-3 px-1">
+        <div class="p-2">
+          <router-link :to="{ name: 'single-apartment', params: { slug: apartment.slug } }"
+            class="text-decoration-none">
+            <div class="card border-0">
+              <!-- controlla immagine -->
+              <div class="">
+                <div class="overflow-hidden rounded-4">
+                  <img v-if="apartment.cover_image" :src="'http://localhost:8000/storage/' + apartment.cover_image"
+                    class="img-fluid rounded-4 my_img_size" alt="...">
+                  <img v-else src="/Placeholder.png" class="img-fluid rounded-4 my_img_size" alt="...">
+                </div>
+              </div>
+              <div class="card-body p-0 pt-3 text-start">
+                <h6 class="card-title fw-medium mb-0">{{ apartment.name }}</h6>
+                <p class="text-black-50 mb-0">Nome host: {{ apartment.user.name }} {{ apartment.user.surname }}</p>
+              </div>
+            </div>
+          </router-link>
+        </div>
       </div>
     </div>
+
+
   </div>
 
-
-
-  <div class="row">
-    <ApartmentItem v-for="currentApartment in apartments" :apartment="currentApartment" :key="currentApartment.id">
-    </ApartmentItem>
-  </div>
-
-
-
-
-
-  {/* <!-- {{ $apartment->cover_image ? asset('storage/' . $apartment->cover_image) : asset('placeholder/Placeholder.png') }} --> */}
 
   <nav class="button-nav d-flex justify-content-center">
 
@@ -208,7 +217,7 @@ export default {
       paginate-buttons-class="paginate-buttons" />
 
   </nav>
-    
+
 
 </template>
 
@@ -216,16 +225,11 @@ export default {
 .my_mini_jumbo {
   height: 60vh;
 }
-.my_mini_jumbo {
-  height: 60vh;
-}
 
 .button-nav {
-.button-nav {
 
-  margin-top: 100px;
+  margin-top: 20px;
   padding-top: 20px;
-  border-top: solid 1px gray;
 
   .pagination-container {
     display: flex;
@@ -237,47 +241,56 @@ export default {
     width: 40px;
     border-radius: 20px;
     cursor: pointer;
-    background-color: rgba($color: #d6d6d6d6, $alpha: 0.3);
+    background-color: transparent;
     border: 1px solid rgb(255, 255, 255);
-    color: rgb(255, 255, 255);
+    color: rgb(0, 0, 0);
   }
 
   .paginate-buttons:hover {
-    background-color: #d8d8d8;
+    background-color: #f7f7f7;
   }
 
   .active-page {
-    background-color: #3498db;
-    border: 1px solid #3498db;
+    background-color: black;
+    border: 1px solid black;
     color: white;
+    pointer-events: none;
   }
 
   .active-page:hover {
-    background-color: #2988c8;
+    background-color: black;
   }
 
 }
-}
-
 
 .card {
 
-  
   cursor: pointer;
 
   .my_img_size {
-    aspect-ratio: 2/1.3;
+    aspect-ratio: 1;
+    object-fit: cover;
 
     &:hover {
       transform: scale(1.1);
+      transition: .3s ease;
+    }
+
+    &:not(:hover) {
+      transform: scale(1);
+      transition: .3s ease;
     }
   }
 
   ;
 
-  &:hover {
-    background-color: rgba(238, 236, 236, 0.123);
+  .card-title,
+  .card-body p {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
+
 
 }
 </style>
