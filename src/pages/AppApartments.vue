71e4a753bfcae4a80d60e332846bba79e447bc45
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-21 14:48:39 +0200

    change style for components

--- src/pages/AppApartments.vue
+++ src/pages/AppApartments.vue
@@ -10,7 +10,7 @@ export default {
     components: {
         ApartmentItem,
         AppHeader,
-       
+
     },
     data() {
         return {
@@ -136,4 +136,76 @@ export default {
     </nav>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.my_mini_jumbo {
+    height: 60vh;
+}
+
+.button-nav {
+
+    margin-top: 20px;
+    padding-top: 20px;
+
+    .pagination-container {
+        display: flex;
+        column-gap: 10px;
+    }
+
+    .paginate-buttons {
+        height: 40px;
+        width: 40px;
+        border-radius: 20px;
+        cursor: pointer;
+        background-color: transparent;
+        border: 1px solid rgb(255, 255, 255);
+        color: rgb(0, 0, 0);
+    }
+
+    .paginate-buttons:hover {
+        background-color: #f7f7f7;
+    }
+
+    .active-page {
+        background-color: black;
+        border: 1px solid black;
+        color: white;
+        pointer-events: none;
+    }
+
+    .active-page:hover {
+        background-color: black;
+    }
+
+}
+
+.card {
+
+    cursor: pointer;
+
+    .my_img_size {
+        aspect-ratio: 1;
+        object-fit: cover;
+
+        &:hover {
+            transform: scale(1.1);
+            transition: .3s ease;
+        }
+
+        &:not(:hover) {
+            transform: scale(1);
+            transition: .3s ease;
+        }
+    }
+
+    ;
+
+    .card-title,
+    .card-body p {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+
+}
+</style>
