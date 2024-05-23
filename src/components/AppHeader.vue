e761f63670401ccf290688428b634c8cad731a49
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-23 15:17:08 +0200

    Fix: sendaddress in appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -53,16 +53,14 @@ export default {
                 const selectedPlace = this.autocomplete[0];
                 this.store.lat = selectedPlace.position.lat;
                 this.store.lon = selectedPlace.position.lon;
-                // console.log(this.sendAddress)
+                // Emissione dell'evento anche per la nuova ricerca
                 this.$emit('address-selected', {
                     address: this.store.address,
                     lat: this.store.lat,
                     lon: this.store.lon,
                     rooms: this.store.rooms,
                     bathrooms: this.store.bathrooms
-
                 });
-                
             }
         },
     }
