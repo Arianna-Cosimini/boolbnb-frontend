a613ec7fc7a0fb9d7f6d6d3a41c9981512665043
vi_ser <vinceulisse@gmail.com>
2024-05-27 09:39:08 +0200

    fixed typo

--- src/pages/LoadingMessage.vue
+++ src/pages/LoadingMessage.vue
@@ -2,32 +2,32 @@
 
 
 export default {
-    name:'LoadingMessage',
-   data() {
-    return {
-      
+    name: 'LoadingMessage',
+    data() {
+        return {
+
         }
     },
     mounted() {
         setTimeout(() => {
-                    this.$router.push('/apartments');
-                  }, 3500);
+            this.$router.push('/apartments');
+        }, 3500);
     }
-  
+
 }
 </script>
 
 <template>
     <div class="container py-5 d-flex align-items-center justify-content-center">
-        
+
         <div class="alert my-bg " role="alert">
             <h4 class="alert-heading">Il tuo messaggio è stato inviato con successo</h4>
-            <p>Il messagio è stato inviato. L'host provvederà a rispondere entro 24 ore</p>
+            <p>Il messaggio è stato inviato. L'host provvederà a rispondere entro 24 ore</p>
             <hr>
             <div class="d-flex align-items-center gap-3">
 
                 <div class="spinner-border" role="status">
-                   <span class="visually-hidden">Loading...</span>
+                    <span class="visually-hidden">Loading...</span>
                 </div>
                 <p class="mb-0">Verrai reindirizzato alla pagina di ricerca</p>
 
@@ -37,6 +37,4 @@ export default {
     </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
