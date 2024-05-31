ffebe13fe86ca49a04c8c11f719029bdb11fe9e6
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-05-31 19:17:25 +0200

    fix:change loader-message

--- src/pages/LoadingMessage.vue
+++ src/pages/LoadingMessage.vue
@@ -18,23 +18,60 @@ export default {
 </script>
 
 <template>
-    <div class="container py-5 d-flex align-items-center justify-content-center">
 
-        <div class="alert my-bg " role="alert">
-            <h4 class="alert-heading">Il tuo messaggio è stato inviato con successo</h4>
-            <p>Il messaggio è stato inviato. L'host provvederà a rispondere entro 24 ore</p>
-            <hr>
-            <div class="d-flex align-items-center gap-3">
+<div class="container d-flex justify-content-center py-5" >
 
-                <div class="spinner-border" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-                <p class="mb-0">Verrai reindirizzato alla pagina di ricerca</p>
+    <div class="alert border-1 border-black my-bg-opacity " role="alert">
+        <div class="p-4">
 
+            <div class="circle d-flex justify-content-center align-content-center mb-2">
+                <i class="fa-solid fa-check check"></i>
             </div>
-        </div>
+            <div class="down">
 
+                <h4 class="alert-heading">Congratulazioni! <br>Il tuo messaggio è stato inviato con successo.</h4>
+    
+                <p>L'host prenderà in considerazione la sua richiesta e si proccuperà di rispondere entro 24 ore.</p>
+                <div class="d-flex align-content-center gap-4">
+                    
+                    <div class="spinner-border" role="status">
+    
+                        <span class="visually-hidden"></span>
+
+                    </div>
+
+                    <p class="mb-0">Verrai reindirizzato alla pagina di ricerca</p>
+
+                </div>
+            
+            </div>
+
+  
+  
+        </div>
+    
     </div>
+</div>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-bg-opacity{
+    background-color: rgb(255, 255, 255);
+    
+   }
+ .circle{
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      background-color: #ff385c40;
+      position: relative;
+    }
+  .check{
+    height: 80px;
+    color: #ff385c;
+  }
+    
+    
+
+</style>
